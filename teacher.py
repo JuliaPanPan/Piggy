864ad1f96357a8279356b34956800f832539b7d7
Julia Pangonis <pangonisj@gilmour.org>
2019-10-18 15:01:55 -0400

    bug fixes

--- teacher.py
+++ teacher.py
@@ -82,28 +82,27 @@ class PiggyParent(gopigo3.GoPiGo3):
     def turn_by_deg(self, deg):
         """Rotates robot relative to it's current heading. If told -20, it
         will rotate left by 20 degrees."""
-​
+
         # get our current angle
         current = self.get_heading()
-​
+
         # calculate delta
         goal = current + deg
-​
+
         # LOOP AROUND THE 360 marker
         goal %= 360
-​
+
         # call turn to deg on the delta
         self.turn_to_deg(goal)
-        
-​
+
     def turn_to_deg(self, deg):
         """Turns to a degree relative to the gyroscope's readings. If told 20, it
         will rotate until the gyroscope reads 20."""
-​
+
         # error check
         goal = abs(deg) % 360
         current = self.get_heading()
-​
+        
         turn = self.right  # connect it to the method without the () to activate
         if (current - goal > 0 and current - goal < 180) or \
             (current - goal < 0 and (360 - goal) + current < 180):
