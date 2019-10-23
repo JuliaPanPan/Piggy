ad6a3f8e3ee42ade0a6195b9631cf78d62fe7b19
Julia Pangonis <pangonisj@gilmour.org>
2019-10-23 10:19:20 -0400

    fixes

--- student.py
+++ student.py
@@ -63,11 +63,11 @@ class Piggy(PiggyParent):
         else:
             print("It's safe to dance")
         for x in range(3):
-            self.dab()
-            self.floss()
+            #self.dab()
+            #self.floss()
             self.whip()
-            self.sprinkler()
-            self.spin()
+            #self.sprinkler()
+            #self.spin()
 
     def safe_to_dance(self):
         """does a 360 distance check and returns true if safe"""
